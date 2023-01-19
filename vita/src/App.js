98685ae49c183dc292ff3ebe4f72ba462c82221c
KORANDID <cjswlvlqkd12@naver.com>
2023-01-19 17:55:24 +0900

    로그인페이지 임시완성

--- vita/src/App.js
+++ vita/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BloodDonation from './pages/BloodDonation';
+import Login from './pages/Login';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <BrowserRouter>
+    <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/BloodDonation" element={<BloodDonation />}></Route>
+        <Route path="/Login" element={<Login/>}></Route>
+        <Route path="/BloodDonation" element={<BloodDonation />}></Route>        
       </Routes>
     </BrowserRouter>
+    
   );
 }
 
---
+++ vita/src/pages/Login.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+
+
+function LogIn() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleChangeId = ({ target: { value } }) => setId(value);
+  const handleChangePassword = ({ target: { value } }) => setPassword(value);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    setDisabled(true);
+    event.preventDefault();
+    await new Promise((r) => setTimeout(r, 1000));
+
+    //back으로 정보 post함
+    fetch('http://localhost:8003/join', {
+      method: 'post',
+      body: JSON.stringify({
+        id: 'userID',
+        password: 'userPW',
+      }),
+    })
+      //보낸거를 문자열 받아 다시 json(객체)으로 변환하여 비교
+      .then((res) => res.json())
+      //회원가입 성공시 실행
+      .then((res) => {
+        navigate('/');
+      })
+      //회원가입 실패시 실행
+      .catch((err) => {
+        setError(err.message);
+        //에러시 Loading메세지 사라지고
+        //에러메세지만 보이도록 설정
+      });
+    setDisabled(false);
+  };  
+  return (    
+    <div>
+    <section>
+    <Styleddiv>로그인</Styleddiv>
+    <Styleddiv1>회원일 경우 로그인 해주세요</Styleddiv1>
+    <hr style={{width:'60%',size:'10',borderWidth:'2px',border:'solid'}}></hr>
+    </section>
+    <section style={{position:'static',display:'flex', justifyContent:'center', alignItems:'center',height:'50vh'}}>
+      <div>
+      <form onSubmit={handleSubmit} >        
+        <Styledinput
+          type="id"
+          placeholder="id"
+          name="id"
+          value={id}
+          onChange={handleChangeId}
+        />
+        <br />
+        <Styledinput
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <br />
+        <StyledButton1 type="submit" disabled={disabled}>
+          로그인
+        </StyledButton1>
+        <br />
+        <StyledButton2 type="submit" disabled={disabled}>
+          회원가입
+        </StyledButton2>
+        <StyledButton3 type="submit" disabled={disabled}>
+          아이디/비밀번호찾기
+        </StyledButton3>
+        <div className="home">{error && <div>{error}</div>}</div>
+      </form>
+      </div>
+    </section>
+    </div>    
+  );
+};
+const Styleddiv = styled.div`
+  position:static;
+  display:flex;
+  justify-Content:center;
+  align-items: flex-start;
+  flex-direction: column;
+  font-size: 48px;
+  font-weight: 700;
+  margin-top:3%;
+  margin-left: 20%;
+`;
+const Styleddiv1 = styled.div`
+  position:static;
+  display:flex;
+  justify-Content:center;
+  align-items: flex-start;
+  flex-direction: column;
+  font-size: 24px;
+  font-weight: 500;
+  margin-left: 20%;
+`;
+const Styledinput = styled.input`
+  width: 593px;
+  height: 68.51px;
+  left: 567px;
+  top: 351.7px;
+`;
+const StyledButton1 = styled.button`
+  background-color: #FF9F9F;
+  color: white;  
+  border-radius: 8px;
+  font-size: 30px;
+  border: none;
+  cursor: pointer;  
+  margin-bottom: 10px;
+  width: 593px;
+  height: 87px;
+  left: 567px;
+  top: 541px;
+`;
+const StyledButton2 = styled.button`
+  background-color: #FFD7D7;
+  color: black;
+  font-size: 15px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right:25px;
+  width: 242px;
+  height: 61px;
+  left: 567px;
+  top: 640px;
+`;
+const StyledButton3 = styled.button`
+  background-color: #FFD7D7;
+  color: black;
+  font-size: 15px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: 325px;
+  height: 61px;
+  left: 835px;
+  top: 640px;
+`;
+export default LogIn;
