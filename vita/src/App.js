2df5d8150350a537cc3ff6d8c5980471189eff27
KORANDID <cjswlvlqkd12@naver.com>
2023-01-30 06:08:02 +0900

    지정헌혈 중간완성

--- vita/src/App.js
+++ vita/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Main';
-import BD_Main from './pages/2. Blood_Donation/BD_Main';
 import Login from './pages/Login';
 import Nav from './components/Nav';
 import SignUp from './pages/SignUp/SignUp';
 import Group from './pages/SignUp/SignUpGroup';
 import Hospital from './pages/SignUp/SignUpHospital';
 import Individual from './pages/SignUp/SignUpIndividual';
+import DBD_PostGeneral from './pages/3. Designated_Blood_Donation/DBD_PostGeneral';
 
 function App() {
   return (
@@ -16,11 +16,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/Login" element={<Login />}></Route>
-        <Route path="/BD_Main" element={<BD_Main />}></Route>
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/Group" element={<Group />}></Route>
         <Route path="/Hospital" element={<Hospital />}></Route>
         <Route path="/Individual" element={<Individual />}></Route>
+        <Route path="/DBD_PostGeneral" element={<DBD_PostGeneral />}></Route>
       </Routes>
     </BrowserRouter>
   );
