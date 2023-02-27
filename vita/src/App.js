0d66f4b8e3089cec9906a5e8a4614e70ab5c5038
KORANDID <cjswlvlqkd12@naver.com>
2023-02-27 17:15:06 +0900

    지정헌혈리스트 주르륵 보이기

--- vita/src/App.js
+++ vita/src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Main';
-import BDMain from './pages/2. Blood_Donation/BD_Main';
 import Login from './pages/Login';
 import Nav from './components/Nav';
-import Navbar from './components/navbar';
 import SignUp from './pages/SignUp/SignUp';
 import Group from './pages/SignUp/SignUpGroup';
 import Hospital from './pages/SignUp/SignUpHospital';
 import Individual from './pages/SignUp/SignUpIndividual';
 import DBDPostGeneral from './pages/3. Designated_Blood_Donation/DBD_PostGeneral';
-<<<<<<< HEAD
 import DBDGeneral from './pages/3. Designated_Blood_Donation/DBD_General';
-=======
-import DBDposting from './pages/3. Designated_Blood_Donation/DBD_Posting';
->>>>>>> db8c042316132b825ba86c95fa2d49cc82f8aba8
+import DBDNews from './pages/3. Designated_Blood_Donation/DBD_News';
 
 function App() {
   return (
@@ -27,13 +22,9 @@ function App() {
         <Route path="/Group" element={<Group />}></Route>
         <Route path="/Hospital" element={<Hospital />}></Route>
         <Route path="/Individual" element={<Individual />}></Route>
-<<<<<<< HEAD
         <Route path="/DBD_PostGeneral" element={<DBDPostGeneral />}></Route>
         <Route path="/DBD_General" element={<DBDGeneral />}></Route>
-=======
-        <Route path="/DBDPostGeneral" element={<DBDPostGeneral />}></Route>
-        <Route path="/DBDposting" element={<DBDposting />}></Route>
->>>>>>> db8c042316132b825ba86c95fa2d49cc82f8aba8
+        <Route path="/DBD_News" element={<DBDNews />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- vita/src/pages/3. Designated_Blood_Donation/DBD_General.js
+++ vita/src/pages/3. Designated_Blood_Donation/DBD_General.js
@@ -4,7 +4,7 @@ import Table from 'react-bootstrap/Table';
 import Accordion from 'react-bootstrap/Accordion';
 import icon from './heart.png';
 
-function DBD_News() {
+function DBD_General() {
     const [error, setError] = useState(null);
 
     const [inputData, setInputData] = useState([{
@@ -16,7 +16,7 @@ function DBD_News() {
         startDate: '',
         DesignatedBloodWriteNumber: '',
         bloodNumber: '',
-    },{}]);
+    }, {}]);
 
     useEffect(() => {
         fetch('http://localhost:8004/user/board/list', {
@@ -26,14 +26,13 @@ function DBD_News() {
             .then((res) => {
                 console.log(res);
                 setInputData(res);
+                console.log(inputData);
             })
             .catch((err) => {
                 setError(err.message);
             });
         console.log(inputData);
     }, []);
-
-
     return (
         <div>
             <section>
@@ -51,66 +50,39 @@ function DBD_News() {
                                 </tr>
                             </thead>
                             <Styledtbody1>
-                                <Styledtr>
-                                    <Styledtd>
-                                        <Accordion.Item eventKey="0">
-                                            <Accordion.Header>
-                                                {inputData[0].title}<br />
-                                                {inputData[0].startDate}<br />
-                                                필요한 혈액형 : {inputData[0].patientBlood}<br />
-                                                혈액 종류 : {inputData[0].bloodType}<br />
-                                                장소 : {inputData[0].hospitalName}
-                                            </Accordion.Header>
-                                            <Accordion.Body colSpan={2}>
-                                                {inputData[0].content}
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                    </Styledtd>
-                                    <Styledtd>
-                                        {inputData[0].bloodNumber}{/* 텍스트 컬러랑 현재 모집인원 /로 표현하기 */}
-                                        <br />
-                                        <Styledimg src={icon} class name="main-icon" alt="logo" />{/* 빈 하트 이미지 추가 후 좋아요 여부로 이미지 다르게 보이기 */}
-                                        <br />
-                                        <button type="button">
-                                            상세보기
-                                        </button>
-                                        <br />
-                                        <button type="button">
-                                            참여하기
-                                        </button>
-                                    </Styledtd>
-                                </Styledtr>
-                            </Styledtbody1>
-                            <Styledtbody1>
-                                <Styledtr>
-                                    <Styledtd>
-                                        <Accordion.Item eventKey="1">
-                                            <Accordion.Header>
-                                                {inputData[1].title}<br />
-                                                {inputData[1].startDate}<br />
-                                                필요한 혈액형 : {inputData[1].patientBlood}<br />
-                                                혈액 종류 : {inputData[1].bloodType}<br />
-                                                장소 : {inputData[1].hospitalName}
-                                            </Accordion.Header>
-                                            <Accordion.Body colSpan={2}>
-                                                {inputData[1].content}
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                    </Styledtd>
-                                    <Styledtd>
-                                        {inputData[1].bloodNumber}{/* 텍스트 컬러랑 현재 모집인원 /로 표현하기 */}
-                                        <br />
-                                        <Styledimg src={icon} class name="main-icon" alt="logo" />{/* 빈 하트 이미지 추가 후 좋아요 여부로 이미지 다르게 보이기 */}
-                                        <br />
-                                        <button type="button">
-                                            상세보기
-                                        </button>
-                                        <br />
-                                        <button type="button">
-                                            참여하기
-                                        </button>
-                                    </Styledtd>
-                                </Styledtr>
+                                {inputData.map((element, index) => {
+                                    return (
+                                        <Styledtr>
+                                            <Styledtd>
+                                                <Accordion.Item eventKey={index}>
+                                                    <Accordion.Header>
+                                                        {element.title}<br />
+                                                        {element.startDate}<br />
+                                                        필요한 혈액형 : {element.patientBlood}<br />
+                                                        혈액 종류 : {element.bloodType}<br />
+                                                        장소 : {element.hospitalName}
+                                                    </Accordion.Header>
+                                                    <Accordion.Body colSpan={2}>
+                                                        {element.content}
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                            </Styledtd>
+                                            <Styledtd>
+                                                {element.bloodNumber}{/* 텍스트 컬러랑 현재 모집인원 /로 표현하기 */}
+                                                <br />
+                                                <Styledimg src={icon} class name="main-icon" alt="logo" />{/* 빈 하트 이미지 추가 후 좋아요 여부로 이미지 다르게 보이기 */}
+                                                <br />
+                                                <button type="button">
+                                                    상세보기
+                                                </button>
+                                                <br />
+                                                <button type="button">
+                                                    참여하기
+                                                </button>
+                                            </Styledtd>
+                                        </Styledtr>
+                                    )
+                                })}
                             </Styledtbody1>
                         </Table>
                     </Accordion>
@@ -160,4 +132,4 @@ const Styledimg = styled.img`
   height: 25px;
   object-fit: cover;
 `;
-export default DBD_News;
+export default DBD_General;
