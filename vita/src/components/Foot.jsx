989a4670f32ce4fa6206f27f7cbf3fecc7c002b4
ADMIN\cjswl <cjswlvlqkd12@naver.com>
2023-03-14 13:23:45 +0900

    footer 완성

---
+++ vita/src/components/Foot.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import icon from './icon.png';
+
+const Footer = () => {
+  return (
+    <Styledfoot>
+      <div style={{ fontSize: '28px' }}>
+        <Styledimg src={icon} class name="main-icon" alt="logo" />
+        <Sp0>
+          헌혈하러 갈래?
+        </Sp0>
+      </div>
+      <div style={{ marginLeft: '5%' }}>
+        <div>부천대학교  |  컴퓨터소프트웨어과  |  6조 졸업작품  |  GITHUBE</div>
+        <br />
+        <div style={{ color: '#777777' }}>팀원 : (팀장)김민지, 김수임, 이민렬</div>
+      </div>
+    </Styledfoot>
+  );
+};
+
+const Sp0 = styled.span`
+  font-weight: bold;
+
+`;
+const Styledfoot = styled.footer`
+  position: static;
+  bottom: 0;
+  width: 100%;
+  height: 150px;
+  background-color: #F5F5F5;
+  margin-top: auto;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Styledimg = styled.img`
+  width: 40px;
+  height: 48px;
+  object-fit: cover; 
+  filter: grayscale(100%);
+  margin-bottom: 10px;
+  margin-right: 10px;
+`;
+
+
+export default Footer;
