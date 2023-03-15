19ce54f81c2a0543460849b3b9f41caf32226a5e
KImSuim <suim7171@naver.com>
2023-03-15 14:05:06 +0900

    상단바, 서브바 UI 구현

--- vita/src/components/Nav.jsx
+++ vita/src/components/Nav.jsx
@@ -6,96 +6,207 @@ import icon from './icon.png';
 
 const Nav = () => {
   return (
-    <Stylednav>
-      <Styleddiv1>
-        <Styledimg src={icon} class name="main-icon" alt="logo" />
-        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem style={{ fontSize: '28px', marginLeft: '0.5%' }}>
-            vita
-          </MenuItem>
-        </Link>
-      </Styleddiv1>
-      <Styleddiv>
-        <Link to="Learn" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>알아보자</MenuItem>
-        </Link>
-      </Styleddiv>
-      <Styleddiv>
-        <Link
-          to="DBD_PostGeneral"
-          style={{ textDecoration: 'none', color: 'white' }}
-        >
-          <MenuItem>헌혈하자</MenuItem>
-        </Link>
-      </Styleddiv>
-      <Styleddiv>
-        <Link to="BD_Story" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>이야기하자</MenuItem>
-        </Link>
-      </Styleddiv>
-      <Styleddiv>
-        <Link to="S_Main" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>봉사하자</MenuItem>
-        </Link>
-      </Styleddiv>
-      <Styleddiv>
-        <Link to="D_Main" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>기부하자</MenuItem>
-        </Link>
-      </Styleddiv>
-      <Styledstate>
-        <Link to="Login" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>로그인</MenuItem>
-        </Link>
-        <div>|</div>
-        <Link to="SignUp" style={{ textDecoration: 'none', color: 'white' }}>
-          <MenuItem>회원가입</MenuItem>
-        </Link>
-      </Styledstate>
-    </Stylednav>
+    <StyledNav>
+      <StyledDiv>
+        <StyledDiv1>
+          <Styledimg src={icon} class name="main-icon" alt="logo" />
+          <Link to="/">
+            <StyledStrBig>
+              <div>VITA</div>
+            </StyledStrBig>
+          </Link>
+        </StyledDiv1>
+
+        <StyledDiv2>
+          <Link to="Learn">
+            <StyledDiv2_1>
+              <div>알아보자</div>
+            </StyledDiv2_1>
+          </Link>
+          <Link to="BDMain">
+            <StyledDiv2_1>
+              <div>헌혈하자</div>
+            </StyledDiv2_1>
+          </Link>
+          <Link to="DBDMain">
+            <StyledDiv2_1>
+              <div>지정헌혈하자</div>
+            </StyledDiv2_1>
+          </Link>
+          <Link to="BDStory">
+            <StyledDiv2_1>
+              <div>이야기하자</div>
+            </StyledDiv2_1>
+          </Link>
+          <Link to="S_Main">
+            <StyledDiv2_1>
+              <div>봉사하자</div>
+            </StyledDiv2_1>
+          </Link>
+          <Link to="D_Main">
+            <StyledDiv2_1>
+              <div>기부하자</div>
+            </StyledDiv2_1>
+          </Link>
+        </StyledDiv2>
+
+        <StyledDiv3>
+          <Link to="Login">
+            <StyledStrSmall>
+              <div>로그인</div>
+            </StyledStrSmall>
+          </Link>
+          <StyledStrSmall>
+            <div> | </div>
+          </StyledStrSmall>
+
+          <Link to="SignUp">
+            <StyledStrSmall>
+              <div>회원가입</div>
+            </StyledStrSmall>
+          </Link>
+        </StyledDiv3>
+      </StyledDiv>
+    </StyledNav>
   );
 };
-const Styledstate = styled.div`
-  flex-direction: row;
+
+const StyledNav = styled.div`
+  /* position: absolute; */
   width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  text-decoration-line: none;
-  margin-right: 15%;
+  height: 94.8px;
+
+  background: #333333;
 `;
-const Stylednav = styled.nav`
-  position: static;
-  bottom: 100;
-  width: 100%;
-  height: 60px;
+
+const StyledDiv = styled.div`
+  /* position: absolute; */
+  /* width: 1180.12px;
+  height: 306px; */
+  margin-left: 370px;
+  margin-top: 24px;
   display: flex;
-  border: solid 2px;
-  background-color: black;
 `;
-const Styleddiv = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  text-decoration-line: none;
-`;
-const Styleddiv1 = styled.div`
-  width: 100%;
+
+const StyledDiv1 = styled.div`
+  /* width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
   text-decoration-line: none;
   margin-left: 15%;
-  font-size: 3rem;
+  font-size: 3rem; */
+  /* position: absolute;
+  width: 120px;
+  height: 46px; */
+  display: flex;
 `;
 const Styledimg = styled.img`
-  width: 20px;
+  /* width: 20px;
   height: 25px;
-  object-fit: cover;
+  object-fit: cover; */
+
+  width: 35px;
+  height: 46px;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+
+  margin-right: 8px;
+`;
+const StyledStrBig = styled.div`
+  /* textdecoration: none;
+  color: white;
+  fontsize: 28px;
+  marginleft: 0.5%; */
+
+  font-family: 'Gmarket Sans TTF';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  /* line-height: 41px; */
+  text-align: center;
+
+  color: #ffffff;
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const StyledDiv2 = styled.div`
+  /* width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-decoration-line: none; */
+
+  /* width: 699px;
+  height: 300px; */
+  margin-left: 119px;
+  margin-right: 119px;
+  margin-top: 6px;
+
+  display: flex;
+`;
+const StyledDiv2_1 = styled.div`
+  /* width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-decoration-line: none; */
+  width: 140px;
+
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  /* line-height: 29px; */
+  /* identical to box height */
+
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const StyledDiv3 = styled.div`
+  /* flex-direction: row;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-decoration-line: none;
+  margin-right: 15%; */
+  display: flex;
+`;
+
+const StyledStrSmall = styled.div`
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  /* line-height: 22px; */
+  /* identical to box height */
+
+  text-align: center;
+
+  color: #ffffff;
+
+  /* Inside auto layout */
+  /* 
+  flex: none;
+  order: 0;
+  flex-grow: 0; */
+  /* margin: auto;
+
+  text-align: center; */
+
+  margin-right: 10px;
 `;
 
 export default Nav;
