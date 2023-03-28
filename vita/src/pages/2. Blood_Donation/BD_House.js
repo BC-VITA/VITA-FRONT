34bcf30b535a41f3b0b603041f0bdc61b40ea3ce
KORANDID <cjswlvlqkd12@naver.com>
2023-03-29 08:33:49 +0900

    03/29

--- vita/src/pages/2. Blood_Donation/BD_House.js
+++ vita/src/pages/2. Blood_Donation/BD_House.js
@@ -1,51 +1,32 @@
 import React, { useEffect, useState } from 'react';
-// import React, { useState } from 'react';
 import styled from 'styled-components';
-
-import { useNavigate } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+import Accordion from 'react-bootstrap/Accordion';
 import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 
 function BD_House() {
-  const [hostipalcall, sethostipalcall] = useState('');
-  const [bloodtype, setbloodtype] = useState('');
-  const [rhtype, setbrhtype] = useState('');
-  const [donationtype, setdonationtype] = useState('');
-  const [bloodproduct, setbloodproduct] = useState('');
 
-  const [disabled, setDisabled] = useState(false);
-
-  const navigate = useNavigate();
-
-  const handleSelect1 = (e) => {
-    setdonationtype(e.target.value);
-  };
-  const handleSelect2 = (e) => {
-    setbloodproduct(e.target.value);
-  };
-  // const handleSelect1 = (e) => {
-  //   setdonationtype(e.target.value);
-  // };
-  // const handleSelect2 = (e) => {
-  //   setbloodtype(e.target.value);
-  // };
-
-  const handleRHMChange = () => {
-    setbrhtype('RH-');
-  };
-  const handleRHPChange = () => {
-    setbrhtype('RH+');
-  };
   const selectList1 = ['전체', '인천', '서울', '경기도', '강원도'];
-  const selectList2 = ['최신순', '마감순'];
-  const handleChangehostipalcall = ({ target: { value } }) =>
-    sethostipalcall(value);
-  const RadioButton = ({ label, value, onChange }) => {
-    return (
-      <label>
-        <input type="radio" checked={value} onChange={onChange} />
-        {label}
-      </label>
-    );
+  const [firstListValue, setFirstListValue] = useState('');
+  const [secondListOptions, setSecondListOptions] = useState([]);
+  function handleFirstListChange(event) {
+    const selectedValue = event.target.value;
+    setFirstListValue(selectedValue);
+
+    if (selectedValue === '전체') {
+      setSecondListOptions(['']);
+    } else if (selectedValue === '서울') {
+      setSecondListOptions(['가', '나', '다']);
+    } else if (selectedValue === '인천') {
+      setSecondListOptions(['가가', '나나', '다다']);
+    } else if (selectedValue === '경기도') {
+      setSecondListOptions(['가가가', '나나나', '다다다']);
+    } else {
+      setSecondListOptions(['가가가가', '나나나나', '다다다다']);
+    }
   };
 
   const [error, setError] = useState(null);
@@ -65,7 +46,7 @@ function BD_House() {
   ]);
 
   useEffect(() => {
-    fetch('http://localhost:8004/user/board/filter', {
+    fetch('http://localhost:8004/blood/house/filter ', {
       method: 'get',
     })
       .then((res) => res.json())
@@ -79,50 +60,170 @@ function BD_House() {
       });
     console.log(inputData);
   }, []);
+
   return (
     <StyledAll>
       <StyledSub>
         <Nav defaultActiveKey="/" className="flex-column">
-          <StyledSubDiv1>헌혈이란</StyledSubDiv1>
+          <StyledSubDiv1>헌혈하자</StyledSubDiv1>
           <StyledSubDiv2>
-            <StyledSubDiv2_1p>
-              <Nav.Link href="/DBD_Main">
-                <StyledSubDiv2_2>헌혈이 집</StyledSubDiv2_2>
+            <StyledSubDiv2One>
+              <Nav.Link href="/BD_Main">
+                <StyledSubDiv2Two>헌혈이란</StyledSubDiv2Two>
               </Nav.Link>
-            </StyledSubDiv2_1p>
-            <StyledSubDiv2_1>
-              <Nav.Link href="/DBD_General">
-                <StyledSubDiv2_2g>지정헌혈하기</StyledSubDiv2_2g>
+            </StyledSubDiv2One>
+            <StyledSubDiv2Onep>
+              <Nav.Link href="/BD_House">
+                <StyledSubDiv2Two2>헌혈하기</StyledSubDiv2Two2>
               </Nav.Link>
-            </StyledSubDiv2_1>
-            <StyledSubDiv2_1>
+            </StyledSubDiv2Onep>
+            <StyledSubDiv2One>
               <Nav.Link href="/DBD_WatchList">
-                <StyledSubDiv2_2g>관심목록</StyledSubDiv2_2g>
+                <StyledSubDiv2Two>관심목록</StyledSubDiv2Two>
               </Nav.Link>
-            </StyledSubDiv2_1>
-            <StyledSubDiv2_1>
+            </StyledSubDiv2One>
+            <StyledSubDiv2One>
               <Nav.Link href="/DBD_News">
-                <StyledSubDiv2_2g>따뜻한 사례</StyledSubDiv2_2g>
+                <StyledSubDiv2Two>따뜻한 사례</StyledSubDiv2Two>
               </Nav.Link>
-            </StyledSubDiv2_1>
+            </StyledSubDiv2One>
           </StyledSubDiv2>
         </Nav>
       </StyledSub>
       <StyledSubcomment>
         <StyledTop>
-          <StyledTitle>지정헌혈이란</StyledTitle>
+          <StyledTitle>지정헌혈하기</StyledTitle>
           <StyledButton>
             <Nav.Link href="/DBDPostGeneral">
-              <StyledButtonDiv>수정하기</StyledButtonDiv>
+              <StyledButtonDiv>작성하기</StyledButtonDiv>
             </Nav.Link>
           </StyledButton>
         </StyledTop>
+
+        <StyledTab>
+          <Tabs>
+            <Tab eventKey="profile" title="일반 사용자">
+              <Tab.Content>
+                <StyledFilter>
+                  <StyledFilterDiv1One>
+                    <StyledFilterDiv1Two>지역선택</StyledFilterDiv1Two>
+                    <select
+                      onChange={handleFirstListChange}
+                      value={firstListValue}
+                      style={{ border: 'none' }}
+                    >
+                      {selectList1.map((item) => (
+                        <option value={item} key={item}>
+                          {item}
+                        </option>
+                      ))}
+                    </select>
+                    <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                    <select value={secondListOptions} onChange={setSecondListOptions}>
+                      {secondListOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </StyledFilterDiv1One>
+                  <StyledFilterDiv1One>
+                    <StyledFilterDiv1Two>&nbsp;기&nbsp;간&nbsp;</StyledFilterDiv1Two>
+                    <select
+                      onChange={handleFirstListChange}
+                      value={firstListValue}
+                      style={{ border: 'none' }}
+                    >
+                      {selectList1.map((item) => (
+                        <option value={item} key={item}>
+                          {item}
+                        </option>
+                      ))}
+                    </select>
+                    <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                    <select value={secondListOptions} onChange={setSecondListOptions}>
+                      {secondListOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </StyledFilterDiv1One>
+                </StyledFilter>
+
+                <section>
+                  <Styleddiv2>
+                    <Accordion defaultActiveKey="0">
+                      <Table striped bordered hover size="sm">
+                        <thead>
+                          <tr>
+                            <th>제목 / 내용</th>
+                            <th>모집인원 및 현황</th>
+                          </tr>
+                        </thead>
+                        <Styledtbody1>
+                          {inputData.map((element, index) => {
+                            return (
+                              <Styledtr>
+                                <Styledtd>
+                                  <Accordion.Item eventKey={index}>
+                                    <Accordion.Header>
+                                      헌혈의집 아이디 : {element.id}
+                                      <br />
+                                      헌혈의집 지역 : {element.area}
+                                      <br />
+                                      헌혈의집 이름 : {element.centerName}
+                                      <br />
+                                      헌혈의집 주소 : {element.bloodHouseAddress}
+                                      <br />
+                                      헌혈의집 전화번호 : {element.bloodHousePhoneNumber}
+                                      <br />
+                                      헌혈의집 위도 : {element.latitude}
+                                      <br />
+                                      헌혈의집 경도 : {element.longitude}
+                                      <br />
+                                      헌혈의집 평일 마감시간 : {element.weekdayTime}
+                                      <br />
+                                      헌혈의집 토요일 마감시간 : {element.saturdayTime}
+                                      <br />
+                                      헌혈의집 일요일 마감시간: {element.sundayRestTime}
+                                      <br />
+                                      헌혈의집 공휴일 마감시간 : {element.restTime}
+                                      <br />
+                                      전혈 가능 여부 : {element.wholeBlood}
+                                      <br />
+                                      혈소판 가능 여부 : {element.plasma}
+                                      <br />
+                                      혈장 가능 여부 : {element.platelet}
+                                    </Accordion.Header>
+                                    <Accordion.Body colSpan={2}>
+                                      지도가 나오게 하기
+                                    </Accordion.Body>
+                                  </Accordion.Item>
+                                </Styledtd>
+                                <Styledtd>
+                                  <button type="button">지도보기</button>
+                                </Styledtd>
+                              </Styledtr>
+                            );
+                          })}
+                        </Styledtbody1>
+                      </Table>
+                    </Accordion>
+                  </Styleddiv2>
+                </section>
+              </Tab.Content>
+            </Tab>
+            <Tab eventKey="home" title="병원">
+              <Tab.Content>skjfjsf</Tab.Content>
+            </Tab>
+          </Tabs>
+        </StyledTab>
       </StyledSubcomment>
-      z<div className="home">{error && <div>{error}</div>}</div>
-    </StyledAll>
+      <div className="home">{error && <div>{error}</div>}</div>
+    </StyledAll >
   );
 }
-
 const StyledAll = styled.div`
   display: flex;
 `;
@@ -156,21 +257,21 @@ const StyledSubDiv2 = styled.div`
   height: 202.5px;
   border: 3px solid #d7d7d7;
 `;
-const StyledSubDiv2_1 = styled.div`
+const StyledSubDiv2One = styled.div`
   border-bottom: 3px solid #d7d7d7;
   background-color: white;
   height: 50px;
   margin-left: 2px;
   margin-right: 2px;
 `;
-const StyledSubDiv2_1p = styled.div`
+const StyledSubDiv2Onep = styled.div`
   border-bottom: 3px solid #ff9f9f;
   background-color: white;
   height: 50px;
   margin-left: 2px;
   margin-right: 2px;
 `;
-const StyledSubDiv2_2 = styled.div`
+const StyledSubDiv2Two2 = styled.div`
   border: solid white 3px;
 
   height: 24px;
@@ -186,7 +287,7 @@ const StyledSubDiv2_2 = styled.div`
 
   color: #333333;
 `;
-const StyledSubDiv2_2g = styled.div`
+const StyledSubDiv2Two = styled.div`
   border: solid white 3px;
 
   height: 24px;
@@ -203,11 +304,34 @@ const StyledSubDiv2_2g = styled.div`
   color: #969696;
 `;
 
+const StyledFilter = styled.div`
+  width: 100%;
+  height: 145px;
+  background: #ffe9e9;
+  margin-bottom: 20px;
+`;
+const StyledFilterDiv1One = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+const StyledFilterDiv1Two = styled.div`
+  font-family: 'Gmarket Sans TTF';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  margin-right: 20px;
+  margin-left: 30px;
+`;
 const StyledSubcomment = styled.div`
   display: block;
   width: 924px;
   margin-left: 65px;
   margin-top: 25px;
+`;
+
+const StyledTab = styled.div`
+  margin-top: 5px;
+  padding-bottom: 500px;
 `;
 
 const StyledTitle = styled.div`
@@ -247,4 +371,23 @@ const StyledButtonDiv = styled.div`
   color: #ffffff;
 `;
 
+const Styleddiv2 = styled.div`
+  /* margin-right: 100px;
+  margin-left: 100px; */
+  text-align: center;
+`;
+const Styledtr = styled.tr`
+  border: none;
+`;
+const Styledtd = styled.td`
+  border: none;
+`;
+const Styledtbody1 = styled.tbody`
+  border: none;
+`;
+const Styledimg = styled.img`
+  width: 30px;
+  height: 25px;
+  object-fit: cover;
+`;
 export default BD_House;
---
+++ vita/src/pages/2. Blood_Donation/BD_test.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
+function BD_test() {
+    //const [id, setId] = useState(''); // 헌혈의 집 ID
+    const [area, setArea] = useState(''); //지역
+    const [centerName, setCenterName] = useState(''); // 센터명
+    const [bloodHouseAddress, setBloodHouseAddress] = useState(''); // 주소
+    const [bloodHousePhoneNumber, setBloodHousePhoneNumber] = useState(''); // 전화번호
+    const [latitude, setLatitude] = useState(''); //위도
+    const [longitude, setLongitude] = useState(''); //경도
+    const [weekdayTime, setWeekdayTime] = useState(''); //평일시간
+    const [saturdayTime, setSaturdayTime] = useState(''); //토요일시간
+    const [sundayRestTime, setSundayRestTime] = useState(''); //일요일 시간 
+    const [restTime, setRestTime] = useState(''); // 공휴일 시간
+
+    const [date, setDate] = useState(''); // 날짜
+    const [wholeBlood, setWholeBlood] = useState(''); //전혈
+    const [plasma, setPlasma] = useState(''); // 혈장
+    const [platelet, setPlatelet] = useState(''); // 혈소판
+
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
+    const handleChangeArea = ({ target: { value } }) => setArea(value);
+    const handleChangeCenterName = ({ target: { value } }) => setCenterName(value);
+    const handleChangeBloodHouseAddress = ({ target: { value } }) => setBloodHouseAddress(value);
+    const handleChangeBloodHousePhoneNumber = ({ target: { value } }) => setBloodHousePhoneNumber(value);
+    const handleChangeLatitude = ({ target: { value } }) => setLatitude(value);
+    const handleChangeLongitude = ({ target: { value } }) => setLongitude(value);
+    const handleChangeWeekdayTime = ({ target: { value } }) => setWeekdayTime(value);
+    const handleChangeSaturdayTime = ({ target: { value } }) => setSaturdayTime(value);
+    const handleChangeSundayRestTime = ({ target: { value } }) => setSundayRestTime(value);
+    const handleChangeRestTime = ({ target: { value } }) => setRestTime(value);
+    const handleChangeDate = ({ target: { value } }) => setDate(value);
+    const handleChangeWholeBlood = ({ target: { value } }) => setWholeBlood(value);
+    const handleChangePlasma = ({ target: { value } }) => setPlasma(value);
+    const handleChangePlatelet = ({ target: { value } }) => setPlatelet(value);
+
+    const handleSubmit = async (event) => {
+        setDisabled(true);
+        event.preventDefault();
+        await new Promise((r) => setTimeout(r, 1000));
+        //back으로 정보 post함
+        fetch('http://localhost:8004/blood/house/register', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+            body: JSON.stringify({
+                area: area,
+                centerName: centerName,
+                bloodHouseAddress: bloodHouseAddress,
+                bloodHousePhoneNumber: bloodHousePhoneNumber,
+                latitude: latitude,
+                longitude: longitude,
+                weekdayTime: weekdayTime,
+                saturdayTime: saturdayTime,
+                sundayRestTime: sundayRestTime,
+                restTime: restTime,
+                date: date,
+                wholeBlood: wholeBlood,
+                plasma: plasma,
+                platelet: platelet,
+            }),
+        })
+            .then((res) => {
+                res.json();
+            })
+            .catch((err) => {
+                setError(err.message);
+            });
+        setDisabled(false);
+        console.log(
+            'data: ' +
+            JSON.stringify({
+                area: area,
+                centerName: centerName,
+                bloodHouseAddress: bloodHouseAddress,
+                bloodHousePhoneNumber: bloodHousePhoneNumber,
+                latitude: latitude,
+                longitude: longitude,
+                weekdayTime: weekdayTime,
+                saturdayTime: saturdayTime,
+                sundayRestTime: sundayRestTime,
+                restTime: restTime,
+                date: date,
+                wholeBlood: wholeBlood,
+                plasma: plasma,
+                platelet: platelet,
+            })
+        );
+    };
+    return (
+        <div style={{ marginLeft: '15%' }}>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>지역</div>
+                    <input type="text" value={area} onChange={handleChangeArea}></input>
+                    <div>센터명</div>
+                    <input type="text" value={centerName} onChange={handleChangeCenterName}></input>
+                    <div>주소</div>
+                    <input type="text" value={bloodHouseAddress} onChange={handleChangeBloodHouseAddress}></input>
+                    <div>전화번호</div>
+                    <input type="text" value={bloodHousePhoneNumber} onChange={handleChangeBloodHousePhoneNumber}></input>
+                    <div>위도</div>
+                    <input type="text" value={latitude} onChange={handleChangeLatitude}></input>
+                    <div>경도</div>
+                    <input type="text" value={longitude} onChange={handleChangeLongitude}></input>
+                    <div>평일시간</div>
+                    <input type="text" value={weekdayTime} onChange={handleChangeWeekdayTime}></input>
+                    <div>토요일시간</div>
+                    <input type="text" value={saturdayTime} onChange={handleChangeSaturdayTime}></input>
+                    <div>일요일시간</div>
+                    <input type="text" value={sundayRestTime} onChange={handleChangeSundayRestTime}></input>
+                    <div>공휴일시간</div>
+                    <input type="text" value={restTime} onChange={handleChangeRestTime}></input>
+                    <div>날짜</div>
+                    <input type="text" value={date} onChange={handleChangeDate}></input>
+                    <div>전혈</div>
+                    <input type="text" value={wholeBlood} onChange={handleChangeWholeBlood}></input>
+                    <div>혈장</div>
+                    <input type="text" value={plasma} onChange={handleChangePlasma}></input>
+                    <div>혈소판</div>
+                    <input type="text" value={platelet} onChange={handleChangePlatelet}></input>
+                </div>
+                <button type="submit">저장</button>
+            </form>
+        </div>
+    );
+}
+
+export default BD_test;
